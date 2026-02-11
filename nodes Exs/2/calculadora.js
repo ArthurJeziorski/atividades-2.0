42b111ae1ffee286ea8924d322dfbc9f959caa61
KATATAL <efja3145@gmail.com>
2026-02-11 15:58:27 -0300

    #2 e #3
    #2 é uma calculador e o #3 é o neymar

---
+++ nodes Exs/2/calculadora.js
@@ -0,0 +1,3 @@
+let numero = 1
+let numerod = numero + 2
+console.log(numerod)
---
+++ nodes Exs/3/personagen.js
@@ -0,0 +1,11 @@
+let nome = "neymar";
+let classe = "lenda";
+let nivel = 9999999;
+let dinheiro = 9999999;
+let idade = 34;
+
+console.log("nome: ", nome)
+console.log("classe: ",classe)
+console.log("Nivel: ", nivel)
+console.log("dinheiro: ",dinheiro)
+console.log("idade: ",idade)
